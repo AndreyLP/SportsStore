4a5587a193c7eff6de17dad2b7183e508d0c6c08
AndreyLP <rowdy.ggl@gmail.com>
2022-01-02 00:31:26 +0300

    Update Cart and Add Order

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -9,15 +9,17 @@ namespace SportsStore.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
-        public CartController(IProductRepository repo)
+        private Cart cart;
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
         public ViewResult Index(string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
@@ -25,32 +27,24 @@ namespace SportsStore.Controllers
         {
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
-            {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
-            }
             return RedirectToAction("Index", new { returnUrl });
         }
         public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
         {
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
-            {
-                Cart cart = GetCart();
                 cart.RemoveLine(product);
-                SaveCart(cart);
-            }
             return RedirectToAction("Index", new { returnUrl });
         }
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
-        }
+        //private Cart GetCart()
+        //{
+        //    Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+        //    return cart;
+        //}
+        //private void SaveCart(Cart cart)
+        //{
+        //    HttpContext.Session.SetJson("Cart", cart);
+        //}
     }
 }
