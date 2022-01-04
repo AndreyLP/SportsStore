99c1a8bad3305eb8600b7a7295a6765e0e66146d
AndreyLP <rowdy.ggl@gmail.com>
2022-01-04 23:38:38 +0300

    Add Finish Order Formation

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -24,6 +24,7 @@ namespace SportsStore
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             //services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
             services.AddMemoryCache();
